eb17c9daaaa3cc8f4d379671bbb673387f6015cf
chlee1252 <chlee1252@gmail.com>
2020-06-13 21:47:12 -0700

    Added Gradient Color

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:subgroup/helper/hexToColor.dart';
+
 
 final shadowColor = Color(0xFF878787).withOpacity(.16);
+final startGradient = hexToColor('#98DE5B');
+final endGradient = hexToColor('#08E1AE');
 
 BorderRadiusGeometry radius = BorderRadius.only(
   topLeft: Radius.circular(30),
