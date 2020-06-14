68fecb3c14ff52f1d9c756caab54d5488f1a76cf
chlee1252 <chlee1252@gmail.com>
2020-06-13 20:58:28 -0700

    Added ShadowColor

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+final shadowColor = Color(0xFF878787).withOpacity(.16);
+
 BorderRadiusGeometry radius = BorderRadius.only(
   topLeft: Radius.circular(30),
   topRight: Radius.circular(30),
