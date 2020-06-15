b0ce4add00d8403e64e1c5b8a0d6c002c6ee7c09
chlee1252 <chlee1252@gmail.com>
2020-06-15 00:16:40 -0700

    Get width/height as arguments

--- lib/widget/gradientContainer.dart
+++ lib/widget/gradientContainer.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:subgroup/constant.dart';
 
 class GradientContainer extends StatelessWidget {
-  GradientContainer({this.child});
+  GradientContainer({this.child, this.width, this.height});
 
   final Widget child;
+  final double width;
+  final double height;
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: this.width,
+      height: this.height,
       decoration: BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.centerRight,
