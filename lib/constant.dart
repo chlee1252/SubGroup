13bd6c44229cae787e53f3272513cbf60baa1dd7
chlee1252 <chlee1252@gmail.com>
2020-06-15 00:17:38 -0700

    Added placeholder Color constant

--- lib/constant.dart
+++ lib/constant.dart
@@ -3,7 +3,10 @@ import 'package:subgroup/helper/hexToColor.dart';
 
 
 final shadowColor = Color(0xFF878787).withOpacity(.16);
+final placeHolderColor = Colors.lightGreenAccent;
 final startGradient = hexToColor('#98DE5B');
+//hexToColor('#98DE5B') green
+//hexToColor('#08E1AE') green
 final endGradient = hexToColor('#08E1AE');
 
 BorderRadiusGeometry radius = BorderRadius.only(
