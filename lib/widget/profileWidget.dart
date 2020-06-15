8184e1fad6f0a7a4f4c39b29f9a285aa42033f3d
chlee1252 <chlee1252@gmail.com>
2020-06-14 23:34:58 -0700

    Changed Container to CircleAvatar

--- lib/widget/profileWidget.dart
+++ lib/widget/profileWidget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:subgroup/constant.dart';
 
 class ProfileWidget extends StatelessWidget {
-
   //TODO: UserProfile Image
   @override
   Widget build(BuildContext context) {
@@ -15,24 +14,18 @@ class ProfileWidget extends StatelessWidget {
         ),
         Column(
           children: [
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                color: Colors.lightGreen,
-                shape: BoxShape.circle,
-                boxShadow: [
-                  BoxShadow(
-                    color: shadowColor,
-                    blurRadius: 1.0,
-                    spreadRadius: 2.0,
-                    offset: Offset(0, 2.0),
-                  ),
-                ],
+            Card(
+              elevation: 10.0,
+              shadowColor: Colors.grey,
+              shape: CircleBorder(),
+              clipBehavior: Clip.antiAlias,
+              child: CircleAvatar(
+                radius: 50,
+                backgroundColor: placeHolderColor,
               ),
             ),
             Container(
-              padding: EdgeInsets.all(8.0),
+              padding: EdgeInsets.all(10.0),
               child: Text(
                 "Changhwan Lee",
                 style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
