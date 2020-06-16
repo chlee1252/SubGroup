def0d53ffc752baf2c3f1e8801e6d933bfc8d77a
chlee1252 <chlee1252@gmail.com>
2020-06-15 23:55:13 -0700

    Added width param

--- lib/widget/circulaGradientrButton.dart
+++ lib/widget/circulaGradientrButton.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class CircularGradientButton extends StatelessWidget {
   CircularGradientButton(
       {this.height,
+        this.width,
       this.gradient,
       this.title,
       this.borderRadius,
       @required this.onPressed});
 
+  final double width;
   final double height;
   final Gradient gradient;
   final String title;
@@ -15,19 +17,23 @@ class CircularGradientButton extends StatelessWidget {
   final BorderRadius borderRadius;
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: this.height,
-        decoration: BoxDecoration(
-          borderRadius: this.borderRadius,
-          gradient: this.gradient,
-        ),
-        child: MaterialButton(
-          child: Text(this.title,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20.0,
-              )),
-          onPressed: this.onPressed,
-        ));
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+          height: this.height,
+          width: this.width,
+          decoration: BoxDecoration(
+            borderRadius: this.borderRadius,
+            gradient: this.gradient,
+          ),
+          child: MaterialButton(
+            child: Text(this.title,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                )),
+            onPressed: this.onPressed,
+          )),
+    );
   }
 }
