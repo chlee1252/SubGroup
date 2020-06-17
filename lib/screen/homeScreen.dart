9cd3a47991011d8787237bcc0776f2a13198c9da
chlee1252 <chlee1252@gmail.com>
2020-06-17 01:13:45 -0700

    Stateful -> Stateless

--- lib/screen/homeScreen.dart
+++ lib/screen/homeScreen.dart
@@ -3,20 +3,16 @@ import 'package:subgroup/animation/delayedAnimation.dart';
 import 'package:subgroup/constant.dart';
 import 'package:subgroup/helper/greets.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:subgroup/screen/createScreen.dart';
 import 'package:subgroup/widget/customAppBar.dart';
 import 'package:subgroup/widget/gradientContainer.dart';
 import 'package:subgroup/widget/profileDrawer.dart';
 
 //TODO: Animation dispose Error Check!
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   static final id = "homescreen";
 
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin{
   @override
   Widget build(BuildContext context) {
     final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
@@ -28,9 +24,11 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
       body: Stack(
         children: [
           GradientContainer(
+            gradientColor: greenGradient,
             child: Column(
               children: [
                 CustomAppBar(
+                  icon: Icons.menu,
                   iconOnPressed: () {
                     _scaffoldKey.currentState.openEndDrawer();
                   },
@@ -41,10 +39,7 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
                     child: Center(
                       child: Text(
                         "Good $greet,\nChanghwan Lee!",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24.0),
+                        style: titleTextStyle,
                       ),
                     ),
                   ),
@@ -58,11 +53,23 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: EdgeInsets.fromLTRB(20, 20, 0, 10),
-                    child: Text(
-                      "Group",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0),
+                    padding: const EdgeInsets.fromLTRB(20, 20, 0, 10),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Group",
+                          style:
+                              TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0),
+                        ),
+                        Spacer(),
+                        IconButton(
+                          padding: EdgeInsets.only(right: 30.0),
+                          icon: Icon(Icons.add, color: Colors.black),
+                          onPressed: () {
+                            Navigator.pushNamed(context, CreateScreen.id);
+                          },
+                        ),
+                      ],
                     ),
                   ),
                   Container(
@@ -80,8 +87,7 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
                   )
                 ],
               ),
-              panelSnapping: false,
-              borderRadius: radius,
+              borderRadius: homeMenuRadius,
               minHeight: size.height * 0.6,
               maxHeight: size.height * 0.8,
             ),
