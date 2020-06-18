f8ecb149c0227510c2cdf5b5f8c69fe2420c23f8
chlee1252 <chlee1252@gmail.com>
2020-06-17 23:53:30 -0700

    Added TextStyle and Green Gradient

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:subgroup/helper/hexToColor.dart';
 
-
 final shadowColor = Color(0xFF878787).withOpacity(.16);
 final placeHolderColor = Colors.lightGreenAccent;
 final startGradient = hexToColor('#98DE5B');
-//hexToColor('#98DE5B') green
-//hexToColor('#08E1AE') green
 final endGradient = hexToColor('#08E1AE');
+final TextStyle titleTextStyle =
+    TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 24.0);
+final List<Color> greenGradient = [
+  startGradient,
+  endGradient,
+];
 
-BorderRadiusGeometry radius = BorderRadius.only(
+BorderRadiusGeometry homeMenuRadius = BorderRadius.only(
   topLeft: Radius.circular(30),
   topRight: Radius.circular(30),
 );
