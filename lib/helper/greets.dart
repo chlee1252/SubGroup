2a0294c1ebf4b5500854ec3ffe6aa125da8bff0e
chlee1252 <chlee1252@gmail.com>
2020-06-18 23:41:33 -0700

    Changed greet range

--- lib/helper/greets.dart
+++ lib/helper/greets.dart
@@ -1,7 +1,7 @@
 String greeting() {
   var hour = DateTime.now().hour;
 
-  if (hour < 12) return 'Morning';
-  if (hour < 17) return 'Afternoon';
+  if (hour >= 5 && hour < 12) return 'Morning';
+  if (hour >= 12 && hour < 17) return 'Afternoon';
   return 'Evening';
 }
