103dda5034242a84cda0bf4dad539daa81cb3af7
chlee1252 <chlee1252@gmail.com>
2020-06-20 00:32:12 -0700

    Added register screen & provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:subgroup/providers/viewPasswordProvider.dart';
 import 'package:subgroup/screen/screens.dart';
 
 void main() {
@@ -9,25 +11,33 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'SubGroup',
-      theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme(
-          Theme.of(context).textTheme,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ViewPassword(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'SubGroup',
+        theme: ThemeData(
+          textTheme: GoogleFonts.poppinsTextTheme(
+            Theme.of(context).textTheme,
+          ),
+          primarySwatch: Colors.green,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        primarySwatch: Colors.green,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
 //      home: LoginScreen(),
 //      initialRoute: LoginScreen.id,
-      home: LoginScreen(),
-      initialRoute: LoginScreen.id,
-      routes: {
-        LoginScreen.id : (context) => LoginScreen(),
-        HomeScreen.id: (context) => HomeScreen(),
-        CreateScreen.id: (context) => CreateScreen(),
-      }
+        home: RegisterScreen(),
+        initialRoute: LoginScreen.id,
+        routes: {
+          LoginScreen.id : (context) => LoginScreen(),
+          RegisterScreen.id: (context) => RegisterScreen(),
+          HomeScreen.id: (context) => HomeScreen(),
+          CreateScreen.id: (context) => CreateScreen(),
+        }
+      ),
     );
   }
 }
