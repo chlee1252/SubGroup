babf315ca3332ccd4a3c908fccd8d143d13ff45b
chlee1252 <chlee1252@gmail.com>
2020-06-20 00:44:52 -0700

    Added LinearGradient

--- lib/constant.dart
+++ lib/constant.dart
@@ -11,6 +11,12 @@ final List<Color> greenGradient = [
   startGradient,
   endGradient,
 ];
+final LinearGradient buttonGradient = LinearGradient(
+  colors: greenGradient,
+  begin: const FractionalOffset(0.2, 0.2),
+  end: const FractionalOffset(1.0, 1.0),
+  tileMode: TileMode.clamp,
+);
 
 BorderRadiusGeometry homeMenuRadius = BorderRadius.only(
   topLeft: Radius.circular(30),
