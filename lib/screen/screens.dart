9a9cc45e62268deb0df9b1c0d1ca76f63b72b8a4
chlee1252 <chlee1252@gmail.com>
2020-06-22 11:29:26 -0700

    Added Welcome Screen

--- lib/screen/screens.dart
+++ lib/screen/screens.dart
@@ -1,3 +1,5 @@
 export 'package:subgroup/screen/homeScreen.dart';
 export 'package:subgroup/screen/loginScreen.dart';
 export 'package:subgroup/screen/createScreen.dart';
+export 'package:subgroup/screen/registerScreen.dart';
+export 'package:subgroup/screen/welcomeScreen.dart';
