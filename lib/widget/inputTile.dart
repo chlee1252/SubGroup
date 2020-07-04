263f5186470957b159f8aae474017736bad06805
chlee1252 <chlee1252@gmail.com>
2020-07-03 19:52:41 -0700

    InputTile Initial commit

---
+++ lib/widget/inputTile.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class InputTile extends StatelessWidget {
+  InputTile({@required this.hintText});
+
+  final String hintText;
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
